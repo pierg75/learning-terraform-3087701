ab93a8613144b7730b1701c6ad3f3a94f4a863f3
Pierguido Lambri <plambri@redhat.com>
2023-11-13 18:43:58 +0000

    Trying to fix the autoscaling
    Signed-off-by: Pierguido Lambri <plambri@redhat.com>

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "autoscaling" {
 
   vpc_zone_identifier   = module.blog_vpc.public_subnets
   target_group_arns     = module.blog_alb.target_groups
-  security_groups = [module.blog_sg.security_group_id]
+  security_groups       = [module.blog_sg.security_group_id]
 
   image_id              = data.aws_ami.app_ami.id
   instance_type         = var.instance_type
@@ -76,7 +76,7 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      target_id        = aws_instance.autoscaling.id
     }
   }
 
